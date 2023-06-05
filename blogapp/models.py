e51f8df162792912adf8c396f5d045ed2805a26d
harshilvagadiya <harshilvagadiya786@gmail.com>
2023-06-05 10:55:26 -0400

    Craete project commit.

---
+++ blogapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    image = models.ImageField(upload_to= 'img', height_field=None, width_field=None, max_length=None)
+    title = models.CharField(max_length=200)
+    body = models.TextField(max_length=500)
+    slug = models.SlugField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Comments....")
+    created = models.DateTimeField(auto_now=True,  null=True)
+    
